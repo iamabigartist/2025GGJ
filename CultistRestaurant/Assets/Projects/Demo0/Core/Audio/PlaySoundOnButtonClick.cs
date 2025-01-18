5a22c556eadd04934e21660ead20661fc3fa76b3
ayi-audio <wleljwjbmc@gmail.com>
2025-01-18 23:44:20 +0800

    接UI音效，音乐新增Stinger

---
+++ CultistRestaurant/Assets/Projects/Demo0/Core/Audio/PlaySoundOnButtonClick.cs
@@ -0,0 +1,24 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Audio
+{
+    public class PlaySoundOnButtonClick : MonoBehaviour
+    {
+        public string eventName;
+        [ReadOnly] public uint uuid;
+        private Button _button;
+        
+        void Start()
+        {
+            _button = gameObject.GetComponent<Button>();
+            _button.onClick.AddListener(PlaySound);
+        }
+        
+        void PlaySound()
+        {
+            uuid = AudioManager.Instance.PostEvent(eventName, AudioManager.Instance.globalInitializer);
+        }
+    }
+}
