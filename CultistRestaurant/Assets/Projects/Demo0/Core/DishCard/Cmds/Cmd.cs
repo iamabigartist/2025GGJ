22155a9456ac9e14b5fa8ae73627a9aead87d3d2
Chua <53459343+iamabigartist@users.noreply.github.com>
2025-01-18 14:06:38 +0800

    into Clue and cmd

--- CultistRestaurant/Assets/Projects/Demo0/Core/DishCard/Cmds/Cmd.cs
+++ CultistRestaurant/Assets/Projects/Demo0/Core/DishCard/Cmds/Cmd.cs
@@ -1,7 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 namespace Projects.Demo0.Core
 {
+public class CmdContext
+{
+	public GameObject TargetGameObj;
+}
 public abstract class Cmd
 {
-	public double DelaySec;
+	public float DelaySec;
+	public abstract void Execute(CmdContext ctx);
+}
+public static class CmdExtensions
+{
+	public static IEnumerator ExecuteCmdList(this IEnumerable<Cmd> cmdList, CmdContext ctx)
+	{
+		foreach (var cmd in cmdList)
+		{
+			yield return cmd.DelaySec > 0 ? new WaitForSeconds(cmd.DelaySec) : null;
+			cmd.Execute(ctx);
+		}
+	}
 }
 }
--- CultistRestaurant/Assets/Projects/Demo0/Core/Localization/MultiLangStr.cs
+++ CultistRestaurant/Assets/Projects/Demo0/Core/Localization/MultiLangStr.cs
@@ -5,6 +5,8 @@ namespace Projects.Demo0.Core.Localization
 [InlineProperty] [HideLabel] [HideReferenceObjectPicker]
 public class MultiLangStr
 {
+	public static LanguageType CurrentLanguage = LanguageType.Chinese;
 	public Dictionary<LanguageType, string> Text = new();
+	public override string ToString() => Text.TryGetValue(CurrentLanguage, out var value) ? value : $"Str of language {CurrentLanguage} Not Found";
 }
 }
