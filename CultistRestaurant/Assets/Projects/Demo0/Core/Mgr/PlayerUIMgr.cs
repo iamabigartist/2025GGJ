843b0101df1bce9ebf888f0fb59cf4e552042a9a
Chua <53459343+iamabigartist@users.noreply.github.com>
2025-01-19 15:45:11 +0800

    菜谱描述功能实装

--- CultistRestaurant/Assets/Projects/Demo0/Core/Mgr/PlayerUIMgr.cs
+++ CultistRestaurant/Assets/Projects/Demo0/Core/Mgr/PlayerUIMgr.cs
@@ -1,5 +1,6 @@
 using System;
 using Projects.Demo0.Core.Utils;
+using UnityEngine;
 using UnityEngine.UI;
 namespace Projects.Demo0.Core.Mgr
 {
@@ -10,6 +11,10 @@ public class PlayerUIMgr : MonoSingleton<PlayerUIMgr>
 	public event Action<bool> OnServeBtnClicked;
 
 	public Text HPText;
+
+	public Button RecipeBtn;
+	public GameObject RecipePanel;
+	public Text RecipeText;
 	public void SetServeBtnInteractive(bool active)
 	{
 		AcceptBtn.interactable = active;
@@ -20,10 +25,15 @@ public class PlayerUIMgr : MonoSingleton<PlayerUIMgr>
 		// 根据HP数量重复爱心表情
 		HPText.text = new('❤', hp);
 	}
+	public void SetRecipeDesc(string desc)
+	{
+		RecipeText.text = desc;
+	}
 	void Start()
 	{
 		AcceptBtn.onClick.AddListener(() => OnServeBtnClicked?.Invoke(true));
 		RefuseBtn.onClick.AddListener(() => OnServeBtnClicked?.Invoke(false));
+		RecipeBtn.onClick.AddListener(() => RecipePanel.SetActive(!RecipePanel.activeSelf));
 	}
 }
 }
