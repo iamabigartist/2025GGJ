b97b546f8f5ad64fc95e9ece7af0ad36f7b4ed31
Chua <53459343+iamabigartist@users.noreply.github.com>
2025-01-19 15:28:54 +0800

    增加本地化描述

--- CultistRestaurant/Assets/Projects/Demo0/Core/DishCard/Doc/DishCardDoc.cs
+++ CultistRestaurant/Assets/Projects/Demo0/Core/DishCard/Doc/DishCardDoc.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Projects.Demo0.Core.Localization;
 using Sirenix.OdinInspector;
 using UnityEngine;
 namespace Projects.Demo0.Core
@@ -6,8 +7,7 @@ namespace Projects.Demo0.Core
 [CreateAssetMenu]
 public class DishCardDoc : SerializedScriptableObject
 {
-	[MultiLineProperty(Lines = 8)]
-	public string RecipeDesc;
+	public MultiLangStr RecipeDesc;
 	public int Priority;
 	public GameObject DishContainerPrefab;
 	public List<DishClueDoc> ClueList = new();
